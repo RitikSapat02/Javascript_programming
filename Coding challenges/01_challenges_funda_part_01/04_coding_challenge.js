d4759d201638e20ed7b4a1ca914f7b05ccfaaea6
Ritik Sapat <118529770+RitikSapat02@users.noreply.github.com>
2022-12-03 17:16:25 +0530

    Delete Coding challenges directory

--- Coding challenges/01_challenges_funda_part_01/04_coding_challenge.js
+++
@@ -1,4 +0,0 @@
-let billValue=275;
-
-let tip = billValue>=50 && billValue<=300 ? 0.15*billValue : 0.2*billValue;
-console.log(`The bill was ${billValue}, the tip was ${tip}, and the total value ${billValue+tip}`);
--- Coding challenges/02_fundame_p_02/2_coding_challenge_02.js
+++
@@ -1,9 +0,0 @@
-const calctip = billValue =>{
-    let tip =  billValue>=50 && billValue<=300 ? 0.15*billValue : 0.2*billValue;
-    return tip;
-}
-
-const bills = [125 , 555, 44];
-const tips = [calctip(bills[0]),calctip(bills[1]), calctip(bills[2])];
-const total = [bills[0]+tips[0], bills[1]+tips[1], bills[2]+tips[2]];
-console.log(bills,tips,total);
