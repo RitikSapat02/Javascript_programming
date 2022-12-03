3711ac260f9e96e9f6304fbbb9afe1fc118fb969
Ritik Sapat <118529770+RitikSapat02@users.noreply.github.com>
2022-12-03 17:12:37 +0530

    Delete 25_ternary_operator.js

--- Basic fundamentals/25_ternary_operator.js
+++
@@ -1,8 +0,0 @@
-const age=15;
-
-age>=18 ? console.log("Adult") : console.log("Child");
-
-const group = age>=18 ? 'Adult' : 'Child';
-console.log(group);
-
-console.log(`I belongs to group ${age>=18 ? 'Adult' : 'Child'}`);
