38ebb38b1aee705c733b1963295730ae12addcc2
Ritik Sapat <118529770+RitikSapat02@users.noreply.github.com>
2022-12-03 17:16:52 +0530

    Delete basic_fundamentals_part_02 directory

--- basic_fundamentals_part_02/25_Strict_mode.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-let hasDriversLicence = false;
-const passTest = true;
-
-if(passTest) hasDriversLicence = true;
-if(hasDriversLicence) console.log('I can drive :D');
--- basic_fundamentals_part_02/35_The_for_loop.js
+++
@@ -1,4 +0,0 @@
-for(let rep = 1; rep<=10; rep++)
-{
-    console.log(`Lifting weights repetition ${rep}`);
-}
