3486320eeb79c0454c4c6dd221c8f77df41f0130
Ritik Sapat <118529770+RitikSapat02@users.noreply.github.com>
2022-12-08 15:42:30 +0530

    Add files via upload

---
+++ Basic fundamentals/05_typeof.js
@@ -0,0 +1,17 @@
+console.log(typeof 10);
+console.log(typeof 4.5);
+console.log(typeof "string");
+console.log(typeof true);
+
+let a=3,b="string",c=true;
+console.log(a,b,c);
+
+let d;
+console.log(typeof d);
+
+console.log(typeof null);  //******
+
+console.log(typeof(NaN));
+
+let f=1/0;
+console.log(f)
