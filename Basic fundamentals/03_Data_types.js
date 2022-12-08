b686a7b8faf5102d4d9729f5fe3d5b955ae8ebfb
Ritik Sapat <118529770+RitikSapat02@users.noreply.github.com>
2022-12-08 15:41:03 +0530

    Add files via upload

---
+++ Basic fundamentals/03_Data_types.js
@@ -0,0 +1,28 @@
+// 7 primitive datatypes
+
+//1) Numbers
+ 
+let a=2;
+let b=5.7;
+console.log(a,b);
+
+//2) string
+let c="jonas";
+let d='ritik';
+console.log(c,d);
+
+//3) Boolean
+let fullAge=true;
+let label=false;
+console.log(label)
+
+//4)Undefined
+let var_address;
+console.log(var_address)
+
+//5)Null
+let r=null;
+console.log(r);
+
+//6)symbol
+//7)BigInt
