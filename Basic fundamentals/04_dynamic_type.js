dc55bc794507784439b5b2b9e4d8f71fc2afb074
Ritik Sapat <118529770+RitikSapat02@users.noreply.github.com>
2022-12-09 19:28:03 +0530

    Delete Basic fundamentals directory

--- Basic fundamentals/04_dynamic_type.js
+++
@@ -1,4 +0,0 @@
-let x;    //undefined
-x=5;      //Number
-x="Ritik" //string
-console.log(x);
--- Basic fundamentals/06_string_concatenation.js
+++
@@ -1,5 +0,0 @@
-let a="Ritik";
-let b=" Sapat";
-console.log(a+b);
-
-console.log("Ritik"+" Sapat"+" is King")
